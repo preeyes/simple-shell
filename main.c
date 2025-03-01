dc9648e3fa3659e6122d1b0a609790c4709dd115
preeyes <meropriyaa@gmail.com>
2025-03-02 03:04:08 +0545

    added parser

--- main.c
+++ main.c
@@ -1,19 +1,36 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
 
 // needs loop to continue asking for command untill user exits the shell
-    
+while (1){
 // needs input parser 
 
-    char input[20];
-
+    char input[1024];
+    
     printf("$  ");
-    scanf("%s", input);
-    printf("You typed: %s/n", input);
+    fgets(input, sizeof(input), stdin);
+    input[strcspn(input, "\n")] = 0;
+
+    if(strcmp(input, "exit") == 0){
+        exit(1);
+    }
+
+    char *args[10];
+    int i = 0;
+
+    char *token = strtok(input, " ");
+    while (token != NULL && i< 9){
+        args[i++] = token;
+        token = strtok (NULL, " ");
+    }
+
+    args[i] = NULL;
 
     // you gotta write a fucntion that breaks the input into 
     // commands and arguments and store them in
@@ -26,20 +43,15 @@ int main(){
     }
 
     else if(p == 0){
-        char *args[] = {"ls", "-l", NULL};
-
         // needs error handling in execvp
 
-        execvp("ls", args);
+        execvp(args[0], args);
 
     }
 
     else{
-
-        // needs to use waitpid for parents to wait // i mean things to do still aint figured out
-        // waitpid(pid, NULL, 0);
-        wait(NULL);
+        waitpid(p, NULL, 0);
     }
-
+}
     return 0;
 }
